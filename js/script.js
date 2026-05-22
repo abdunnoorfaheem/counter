2e9c06184bfa737b3a3774079161acf9efad91fe
abdunnoorfaheem <abdunnoor3030@gmail.com>
2026-05-22 10:07:55 +0600

    couter logic added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("hello")
+let plus=document.getElementById("plus");
+let digit=document.getElementById("digit");
+let minus=document.getElementById("minus");
+
+
+
+let count=0;
+
+plus.addEventListener("click",()=>{
+    count++;
+    digit.innerText=count;
+})  
+minus.addEventListener("click",()=>{
+    if(count>0){
+        count--;
+        
+    digit.innerText=count;
+    }
+   
+})
